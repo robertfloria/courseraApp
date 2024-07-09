a00091da3a86e452850876e7f3f6469566e8828b
Robert Nicolae Floria <robertfloria27@gmail.com>
2024-07-09 11:37:34 +0300

    navigation rewrited to use screens in differents folders

--- little-lemon-app/app/_layout.tsx
+++ little-lemon-app/app/_layout.tsx
@@ -3,7 +3,7 @@ import { Stack } from "expo-router";
 export default function RootLayout() {
   return (
     <Stack>
-      <Stack.Screen name="welcomeScreen" options={{ title: 'Welcome' }} />
+      <Stack.Screen name="(welcomeScreen)" options={{ title: 'Welcome' }} />
       <Stack.Screen name="subscribeScreen" options={{ title: 'Subscribe' }} />
     </Stack>
   );
